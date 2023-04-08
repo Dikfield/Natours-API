d83b579c4e2542a7d2b46569dcbf3f8bc1a99243
Diogo <dinash@outlook.com>
2023-04-08 19:58:25 +0200

    added email handling

--- utils/email.js
+++ utils/email.js
@@ -1,23 +1,56 @@
 const nodemailer = require('nodemailer');
+const pug = require('pug');
+const htmlToText = require('html-to-text');
 
-const sendEmail = async (options) => {
-  const transporter = nodemailer.createTransport({
-    host: process.env.EMAIL_HOST,
-    port: process.env.EMAIL_PORT,
-    auth: {
-      user: process.env.EMAIL_USERNAME,
-      pass: process.env.EMAIL_PASSWORD,
-    },
-  });
+module.exports = class Email {
+  constructor(user, url) {
+    this.to = user.email;
+    this.firstName = user.name.split(' ')[0];
+    this.url = url;
+    this.from = `Diogo Cortez <${process.env.EMAIL_FROM}>`;
+  }
 
-  const mailOptions = {
-    from: 'Diogo <dinash@outlook.com>',
-    to: options.email,
-    subject: options.subject,
-    text: options.message,
-  };
+  createTransport() {
+    if (process.env.NODE_ENV === 'production') {
+      //Sendgrid
+      return 1;
+    }
 
-  await transporter.sendMail(mailOptions);
+    return nodemailer.createTransport({
+      host: process.env.EMAIL_HOST,
+      port: process.env.EMAIL_PORT,
+      auth: {
+        user: process.env.EMAIL_USERNAME,
+        pass: process.env.EMAIL_PASSWORD,
+      },
+    });
+  }
+  //Send the actual email
+  async send(template, subject) {
+    // 1) Render HTML based on a pug template
+    const html = pug.renderFile(
+      `${__dirname}/../views/emails/${template}.pug`,
+      {
+        firstname: this.firstName,
+        url: this.url,
+        subject,
+      }
+    );
+
+    // 2) Define email options
+    const mailOptions = {
+      from: this.from,
+      to: this.to,
+      subject,
+      html,
+      text: htmlToText.fromString(html),
+    };
+
+    // 3) Create a transport and send email
+    await this.createTransport().sendMail(mailOptions);
+  }
+
+  async sendWecome() {
+    await this.send('welcome', 'welcome to the Natours Family!');
+  }
 };
-
-module.exports = sendEmail;
